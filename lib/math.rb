a8cee29803078dcb4d9f5f59c8fe67b569c2556e
Yu Li <yli0607x@gmail.com>
2018-10-03 19:49:39 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -7,17 +7,21 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
+  num1 / num2
 end
 
 def multiplication(num1, num2)
+  num1 * num2
 end
 
 def modulo(num1, num2)
+  num1 % num2
 end
 
 def square_root(num)
+  Math.sqrt(num)
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  #Hint:  __ + (( __ * __ ) / __ )
+ num1 + ((num 2 * num 3) / num 4)
 end
